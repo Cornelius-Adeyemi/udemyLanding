49935db6d847235c6e63fa5ea36138928df2b2b3
Adebisi Adeyem <aadebisi119@gmail.com>
2022-12-04 16:28:39 -0600

    first commit

---
+++ data.js
@@ -0,0 +1,142 @@
+
+
+
+let myArray = [
+{
+    image:"./image/courseImage/Capture1.PNG",
+    header:"Learn Python: The Complete Python Programming CourseLearn A-Z everything about Python, from the basics, to advanced topics like Python GUI, Python Data Analysis, and more!Rating: 4.3 out of 53959 reviews14.5 total hours146 lecturesAll LevelsCurrent price: ₦3,200Original price: ₦14,900",
+    auto:"Adebisi Adeyemi",
+    star:4.3,
+    price:3900,
+    people:4100,
+    slashPrice: 1400
+},
+{
+    image:"./image/courseImage/Capture2.PNG",
+    header:"Learn Python: The Complete Python Programming CourseLearn A-Z everything about Python, from the basics, to advanced topics like Python GUI, Python Data Analysis, and more!Rating: 4.3 out of 53959 reviews14.5 total hours146 lecturesAll LevelsCurrent price: ₦3,200Original price: ₦14,900",
+    auto:"Adebisi Adeyemi",
+    star:4.3,
+    price:3900,
+    people:4100,
+    slashPrice: 1400
+},
+{
+    image:"./image/courseImage/Capture3.PNG",
+    header:"Learn Python: The Complete Python Programming CourseLearn A-Z everything about Python, from the basics, to advanced topics like Python GUI, Python Data Analysis, and more!Rating: 4.3 out of 53959 reviews14.5 total hours146 lecturesAll LevelsCurrent price: ₦3,200Original price: ₦14,900",
+    auto:"Adebisi Adeyemi",
+    star:4.3,
+    price:3900,
+    people:4100,
+    slashPrice: 1400
+},
+{
+    image:"./image/courseImage/Capture4.PNG",
+    header:"Learn Python: The Complete Python Programming CourseLearn A-Z everything about Python, from the basics, to advanced topics like Python GUI, Python Data Analysis, and more!Rating: 4.3 out of 53959 reviews14.5 total hours146 lecturesAll LevelsCurrent price: ₦3,200Original price: ₦14,900",
+    auto:"Adebisi Adeyemi",
+    star:4.3,
+    price:3900,
+    people:4100,
+    slashPrice: 1400
+},
+{
+    image:"./image/courseImage/Capture5.PNG",
+    header:"Learn Python: The Complete Python Programming CourseLearn A-Z everything about Python, from the basics, to advanced topics like Python GUI, Python Data Analysis, and more!Rating: 4.3 out of 53959 reviews14.5 total hours146 lecturesAll LevelsCurrent price: ₦3,200Original price: ₦14,900",
+    auto:"Adebisi Adeyemi",
+    star:4.3,
+    price:3900,
+    people:4100,
+    slashPrice: 1400
+},
+{
+    image:"./image/courseImage/Capture1.PNG",
+    header:"Learn Python: The Complete Python Programming CourseLearn A-Z everything about Python, from the basics, to advanced topics like Python GUI, Python Data Analysis, and more!Rating: 4.3 out of 53959 reviews14.5 total hours146 lecturesAll LevelsCurrent price: ₦3,200Original price: ₦14,900",
+    auto:"Adebisi Adeyemi",
+    star:4.3,
+    price:3900,
+    people:4100,
+    slashPrice: 1400
+},
+{
+    image:"./image/courseImage/Capture2.PNG",
+    header:"Learn Python: The Complete Python Programming CourseLearn A-Z everything about Python, from the basics, to advanced topics like Python GUI, Python Data Analysis, and more!Rating: 4.3 out of 53959 reviews14.5 total hours146 lecturesAll LevelsCurrent price: ₦3,200Original price: ₦14,900",
+    auto:"Adebisi Adeyemi",
+    star:4.3,
+    price:3900,
+    people:4100,
+    slashPrice: 1400
+},
+{
+    image:"./image/courseImage/Capture3.PNG",
+    header:"Learn Python: The Complete Python Programming CourseLearn A-Z everything about Python, from the basics, to advanced topics like Python GUI, Python Data Analysis, and more!Rating: 4.3 out of 53959 reviews14.5 total hours146 lecturesAll LevelsCurrent price: ₦3,200Original price: ₦14,900",
+    auto:"Adebisi Adeyemi",
+    star:4.3,
+    price:3900,
+    people:4100,
+    slashPrice: 1400
+},
+{
+    image:"./image/courseImage/Capture4.PNG",
+    header:"Learn Python: The Complete Python Programming CourseLearn A-Z everything about Python, from the basics, to advanced topics like Python GUI, Python Data Analysis, and more!Rating: 4.3 out of 53959 reviews14.5 total hours146 lecturesAll LevelsCurrent price: ₦3,200Original price: ₦14,900",
+    auto:"Adebisi Adeyemi",
+    star:4.3,
+    price:3900,
+    people:4100,
+    slashPrice: 1400
+},
+
+
+]
+
+export  let course = [
+
+    {
+       type:"python",
+       header:"Expand your career opportunities with Python",
+       preview:"Take one of Udemy’s range of Python courses and learn how to code using this incredibly useful language. Its simple syntax and readability makes Python perfect for Flask, Django, data science, and machine learning. You’ll learn how to build everything from games to sites to apps. Choose from a range of courses that will appeal to",
+       botton:"explore python",
+       myArray:myArray
+
+    },
+    {
+        type:"Excel",
+        header:"Analyze and visualize data with Excel",
+        preview:"Take a Microsoft Excel course from Udemy, and learn how to use this industry-standard software. Real-world experts will show you the basics like how to organize data into sheets, rows and columns, and advanced techniques like creating complex dynamic formulas. Both small businesses and large companies use Excel to",
+       botton:"explore excel",
+       myArray:myArray
+    },
+    {
+        type:"Web development",
+        header:"Build websites and applications with Web Development",
+        preview:"The world of web development is as wide as the internet itself. Much of our social and vocational lives play out on the internet, which prompts new industries aimed at creating, managing, and debugging the websites and applications that we increasingly rely on.",
+       botton:"Explore web development",
+       myArray:myArray
+    },
+    {
+        type:"javascript",
+        header:"Grow your software development skills with JavaScript",
+        preview:"JavaScript is a text-based computer programming language used to make dynamic web pages. A must-learn for aspiring web developers or programmers, JavaScript can be used for features like image carousels, displaying countdowns and timers, and playing media on a webpage. With JavaScript online classes, you can learn to build",
+       botton:"Expore javascript",
+       myArray:myArray
+    },
+    {
+        type:"data science",
+        header:"Lead data-driven decisions with Data Science",
+        preview:"Data science application is an in-demand skill in many industries worldwide — including finance, transportation, education, manufacturing, human resources, and banking. Explore data science courses with Python, statistics, machine learning, and more to grow your knowledge. Get data science training if you’re into research, statistics, and analytics.",
+       botton:"expolore data science",
+       myArray:myArray
+    },
+    {
+        type: "AWS Certification",
+        header:"Become an expert in cloud computing with AWS Certification",
+        preview:"Prep for your AWS certification with an AWS course on Udemy. Learn the fundamentals of AWS such as working with a serverless platform, the various frameworks, security and more. With these courses, you’ll build the valuable skills you need to implement cloud initiatives — and open up new career opportunities. If you want to",
+       botton:"Explore AWS certification",
+       myArray:myArray
+    },
+    {
+        type:"Drawing",
+        header:"Expand your creative skillset with Drawing",
+        preview:"Want to start drawing for fun or take your craft to the next level? Explore our online drawing classes and learn pencil drawing, figure drawing, cartoon drawing, character drawing for cartoons and anime, illustration, sketching, shading and more. Take an overview course on the fundamentals of drawing or zero in on an area",
+       botton:"Explore drawing",
+       myArray:myArray
+    },
+]
---
+++ javascript.js
@@ -0,0 +1,232 @@
+
+import {course} from "./data.js"
+let listItems = document.querySelectorAll("#firstSection li");
+const courseHeader = document.getElementById("courseHeader");
+const coursePreview = document.getElementById("coursePreview")
+const courseButton = document.getElementById("courseButton")
+let courseInTitle = "python";
+
+const courseContainer = document.getElementById("courseCardList");
+let number;
+let courseInView = course.find((item)=>{
+    return item.type.toLowerCase() === courseInTitle;
+});
+
+// addcourse
+
+function  courseCard(){
+
+    courseInView.myArray.forEach((item, index)=>{
+      
+        // create element
+    let cardMainDiv = document.createElement("div");
+    cardMainDiv.setAttribute("id", "eachCard")
+    let cardImg = document.createElement("img");
+    cardImg.src=item.image;
+    cardMainDiv.appendChild(cardImg);
+    let cardh3 = document.createElement("h3");
+    cardh3.textContent = item.header.substring(0,41);
+    cardMainDiv.appendChild(cardh3);
+    let cardp = document .createElement("p");
+    cardp.setAttribute("id", "cardName");
+    cardp.textContent = item.auto;
+    cardMainDiv.appendChild(cardp)
+    let innerDivOne = document.createElement("div");
+    innerDivOne.setAttribute("id","cardRating") 
+    cardMainDiv.appendChild(innerDivOne);
+    let innerSpanOne = document.createElement("span");
+    innerSpanOne.classList.add("ratingSpan");
+    innerSpanOne.textContent =item.star;
+    innerDivOne.appendChild(innerSpanOne);
+    let innerSpanImg = document.createElement("span");
+    innerSpanImg.setAttribute("id", "imageSpan");
+    innerDivOne.appendChild(innerSpanImg);
+    let innerSpanTwo = document.createElement("span");
+    innerSpanTwo.classList.add("cardPeople");
+    innerSpanTwo.textContent = "("+item.people + ")";
+    innerDivOne.appendChild(innerSpanTwo);
+    let innerDivTwo = document.createElement("div");
+    innerDivTwo.setAttribute("id","cardPrice");
+    cardMainDiv.appendChild(innerDivTwo);
+    let innerSpanThree = document.createElement("span");
+    innerSpanThree.classList.add("actualPrice");
+    innerSpanThree.textContent=item.price;
+    innerDivTwo.appendChild(innerSpanThree);
+    let innerSpanFour = document.createElement("span");
+    innerSpanFour.classList.add("cancelPrice");
+    innerSpanFour.textContent=item.slashPrice;
+    innerDivTwo.appendChild(innerSpanFour);
+
+    // append
+    courseContainer.appendChild(cardMainDiv);
+      
+    })
+
+}
+
+courseCard()
+
+let slide =0;
+let cardArray = document.querySelectorAll("#eachCard");
+function changeDisplay(){
+    
+    cardArray.forEach((item,index)=>{
+        if(index < slide){
+          item.classList.add("slideLeft");
+           
+        }else if(index >= slide && index < slide + number){
+          item.classList.remove("slideLeft");
+          item.classList.remove("slideRight");
+          item.classList.remove("firstItem");
+          item.classList.remove("lastItem")
+          if(index ===slide){
+           item.classList.add("firstItem");
+          }else if(index === (slide + number - 1)){
+            item.classList.add("lastItem")
+          }
+
+        }else{
+          item.classList.add("slideRight");
+        }
+      
+      
+      })
+      
+    }
+
+// to change the number of course in view
+
+function screenFunction(){
+    
+
+    if(window.innerWidth > 1150){
+   
+     number =5
+    }else if( window.innerWidth > 920 && window.innerWidth < 1150){
+    //4
+  
+    number =4;
+    }else if( window.innerWidth > 644 && window.innerWidth < 920){
+     //3
+     number =3;
+     
+     }else if( window.innerWidth < 644 ){
+         //4
+         number =2;
+         
+     }
+
+     changeDisplay()
+    
+ }
+
+ screenFunction()
+
+ 
+
+ // to fill in the header information 
+
+function fillInData(){
+    
+    courseHeader.textContent = courseInView.header;
+    coursePreview.textContent= courseInView.preview;
+    courseButton.textContent= courseInView.botton;
+    screenFunction();
+}
+
+
+fillInData();
+console.log("here",number)
+// to add eventlistener to the list naviagetion 
+
+listItems.forEach((item)=>{
+    item.addEventListener("click", chnageCourseInTitle);
+    changeFocus()
+})
+
+// change course title
+
+function chnageCourseInTitle(e){
+    if(e.target.textContent.toLowerCase()===courseInTitle){
+    
+    }else{
+ courseInTitle = e.target.textContent.toLowerCase();
+ changeCourseInView();
+ changeFocus()
+ fillInData()
+    }
+}
+// change course in view
+function changeCourseInView(e){
+
+    courseInView = course.find((item)=>{
+        return item.type.toLowerCase() === courseInTitle;
+    })
+
+}
+
+
+//change focus of the list 
+function changeFocus(){
+    listItems.forEach((item)=>{
+        if(item.textContent.toLowerCase() === courseInTitle){
+            
+            item.style.color = "rgba(0, 0, 0,1)"
+        }else{
+            item.style.color = "rgba(0, 0, 0,0.6)"
+        }
+    })
+}
+
+
+
+
+
+
+let greater = document.getElementById("greaterThan");
+let lessThan = document.getElementById("lessThan");
+
+window.addEventListener("resize", screenFunction)
+
+
+
+function increaseSlide(e){
+  let element = (e)?e.currentTarget : {};
+ if(element.id === "lessThan"){
+    if(slide===0){
+
+    }else if(slide>0){
+
+        slide--
+    }
+ }else if(element.id==="greaterThan"){
+    if(slide === cardArray.length - (number - 1)- 1){
+
+    }else if(slide < cardArray.length - (number - 1)- 1){
+        slide++
+    }
+ }
+ if(slide===0){
+    lessThan.classList.add("deactivate");
+ }else if(slide > 0 && slide< cardArray.length - (number - 1)- 1 ){
+    lessThan.classList.remove("deactivate");
+    greater.classList.remove("deactivate");
+ }else if(slide === cardArray.length - (number - 1)- 1 ){
+    greater.classList.add("deactivate");
+ }
+ console.log("slide",slide, cardArray.length, cardArray.length - (number - 1)- 1)
+ changeDisplay()
+    
+}
+
+increaseSlide()
+
+
+let courseMove = document.querySelectorAll(".courseMove");
+
+courseMove.forEach((item)=>{
+    item.addEventListener("click",increaseSlide)
+})
+
+
+
